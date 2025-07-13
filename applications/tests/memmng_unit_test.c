311bda2dbef6128f85671e23498ae929e99ba107
PaulFirs <makloun@hotmail.com>
2025-07-13 22:50:36 +0300

    Fix mem test for runit

--- applications/tests/memmng_unit_test.c
+++ applications/tests/memmng_unit_test.c
@@ -4,28 +4,24 @@
 #include <stdint.h>
 #include "rhs.h"
 #include "cli.h"
+#include "runit.h"
 
 #define TAG "mem_test"
 
 void rhs_memmgr_test(char* args, void* context)
 {
     void* ptr;
+    runit_counter_assert_passes = 0;
+    runit_counter_assert_failures = 0;
 
     // allocate memory case
     ptr = malloc(100);
-    if (ptr == NULL)
-    {
-        RHS_LOG_D(TAG, "fault");
-        return;
-    }
+    runit_assert(ptr != NULL);
+    
     // test that memory is zero-initialized after allocation
     for (int i = 0; i < 100; i++)
     {
-        if (0 != ((uint8_t*) ptr)[i])
-        {
-        RHS_LOG_D(TAG, "fault");
-        return;
-        }
+        runit_assert(((uint8_t*) ptr)[i] == 0);
     }
     free(ptr);
 
@@ -33,41 +29,27 @@ void rhs_memmgr_test(char* args, void* context)
     ptr = malloc(100);
     memset(ptr, 66, 100);
     ptr = realloc(ptr, 200);
-    if (ptr == NULL)
-    {
-        RHS_LOG_D(TAG, "fault");
-        return;
-    }
+    runit_assert(ptr != NULL);
 
     // test that memory is really reallocated
     for (int i = 0; i < 100; i++)
     {
-        if (66 != ((uint8_t*) ptr)[i])
-        {
-        RHS_LOG_D(TAG, "fault");
-        return;
-        }
+        runit_assert(((uint8_t*) ptr)[i] == 66);
     }
 
     free(ptr);
 
     // allocate and zero-initialize array (calloc)
     ptr = calloc(100, 2);
-    if (ptr == NULL)
-    {
-        RHS_LOG_D(TAG, "fault");
-        return;
-    }
+    runit_assert(ptr != NULL);
     for (int i = 0; i < 100 * 2; i++)
     {
-        if (0 != ((uint8_t*) ptr)[i])
-        {
-        RHS_LOG_D(TAG, "fault");
-        return;
-        }
+        runit_assert(((uint8_t*) ptr)[i] == 0);
     }
     free(ptr);
     RHS_LOG_D(TAG, "test mem OK");
+
+    runit_report();
 }
 
 void rhs_memmgr_test_start_up(void)

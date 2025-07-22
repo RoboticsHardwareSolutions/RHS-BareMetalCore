e320e79644c3cb6fc21d3d4f99be91067d0ea712
PaulFirs <makloun@hotmail.com>
2025-07-22 17:10:23 +0300

    Decomposition of rplc templates

---
+++ device_templates/plc_m.cmake
@@ -0,0 +1,69 @@
+set(RPLC_M ON)
+
+## set HAL
+set(RHS_HAL_SERIAL ON)
+set(RHS_HAL_SPEAKER ON)
+set(RHS_HAL_CAN ON)
+set(RHS_HAL_USB ON)
+
+## set SERVICES
+if(NOT RHS_SERVICE_NOTIFICATION_DISABLE)
+    set(RHS_SERVICE_NOTIFICATION ON)
+endif()
+if(NOT RHS_SERVICE_USB_SERIAL_BRIDGE_DISABLE)
+    set(RHS_SERVICE_USB_SERIAL_BRIDGE ON)
+endif()
+if(NOT RHS_SERVICE_CAN_OPEN_DISABLE)
+    set(RHS_SERVICE_CAN_OPEN ON)
+endif()
+
+## set TESTS
+if(NOT RHS_TEST_MEMMNG_DISABLE)
+    set(RHS_TEST_MEMMNG ON)
+endif()
+if(NOT RHS_TEST_LOG_SAVE_DISABLE)
+    set(RHS_TEST_LOG_SAVE ON)
+endif()
+
+## set LAUNCHER
+set(RHS_LAUNCH_GENERATOR_ENABLE ON)
+set(DEVICE_TYPE "STM32F103RE")
+set(DEVICE_SVD_FILE "STM32F103.svd")
+
+
+## compiler config
+if(NOT LINKER_SCRIPT_NAME)
+    set(LINKER_SCRIPT_NAME STM32F103RETX_FLASH.ld)
+endif()
+
+set(LINKER_SCRIPT ${CMAKE_SOURCE_DIR}/${LINKER_SCRIPT_NAME})
+
+add_compile_definitions(ARM_MATH_CM3;ARM_MATH_MATRIX_CHECK;ARM_MATH_ROUNDING)
+#add_compile_options(-mfloat-abi=hard -mfpu=fpv4-sp-d16)
+#add_link_options(-mfloat-abi=hard -mfpu=fpv4-sp-d16)
+add_link_options(-Wl,-gc-sections,--print-memory-usage)
+add_link_options(-mcpu=cortex-m3 -mthumb -mthumb-interwork)
+#add_link_options(-T ${LINKER_SCRIPT})
+
+#Uncomment for software floating point
+#add_compile_options(-mfloat-abi=soft)
+
+add_compile_options(-mcpu=cortex-m3 -mthumb -mthumb-interwork)
+add_compile_options(-ffunction-sections -fdata-sections -fno-common -fmessage-length=0)
+
+# uncomment to mitigate c++17 absolute addresses warnings
+#set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-register")
+
+include_directories(
+    core/inc
+    core/inc/f1
+    thirdparty/stm32f1_hal/Inc
+    thirdparty/stm32f1_hal/Inc/Legacy
+    thirdparty/stm32f1_cmsis/Include
+    thirdparty/cmsis/CMSIS/Core/Include
+)
+
+add_definitions(-DUSE_HAL_DRIVER -DSTM32F103xE -DRPLC_M)
+
+file(GLOB_RECURSE SOURCES "core/src/syscalls.c" "core/src/sysmem.c" "core/src/f1/*.*" "thirdparty/stm32f1_hal/*.*" "thirdparty/stm32f1_cmsis/Source/Templates/gcc/startup_stm32f103xe.s")
+list(FILTER SOURCES EXCLUDE REGEX "_template[.]c$")

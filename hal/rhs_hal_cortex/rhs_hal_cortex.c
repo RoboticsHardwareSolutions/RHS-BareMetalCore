47007825f09570cf510d934ad65284cbedd7d82a
PaulFirs <makloun@hotmail.com>
2026-04-26 17:05:43 +0300

    Added mpu config for sram2 to set non-cachable sector

--- hal/rhs_hal_cortex/rhs_hal_cortex.c
+++ hal/rhs_hal_cortex/rhs_hal_cortex.c
@@ -3,6 +3,7 @@
 
 #if defined(STM32F765xx)
 #    include "stm32f7xx.h"
+#    include "stm32f7xx_hal_cortex.h"
 #elif defined(STM32F407xx)
 #    include "stm32f4xx.h"
 #elif defined(STM32F103xE)
@@ -14,9 +15,46 @@
 
 #define RHS_HAL_CORTEX_INSTRUCTIONS_PER_MICROSECOND (SystemCoreClock / 1000000)
 
+/* Linker script symbols for SRAM2 region */
+extern uint32_t _sram2_start;
+extern uint32_t _sram2_end;
+extern uint32_t _sram2_size;
+
+#ifdef STM32F765xx
+static void rhs_hal_cortex_configure_mpu(void)
+{
+    MPU_Region_InitTypeDef mpu = {0};
+
+    HAL_MPU_Disable();
+
+    /*
+     * Mark SRAM2 (0x2007C000..0x20080000, 16KB) as non-cacheable.
+     * MPU requires power-of-two region size and alignment.
+     * Base address and size are taken from linker script symbols.
+     * The first subregion (2KB) covers .noinit_log and is excluded
+     * from this region (falls through to background = cacheable).
+     */
+    mpu.Enable           = MPU_REGION_ENABLE;
+    mpu.Number           = MPU_REGION_NUMBER7;
+    mpu.BaseAddress      = (uint32_t)&_sram2_start;
+    mpu.Size             = MPU_REGION_SIZE_16KB;
+    mpu.SubRegionDisable = 0x03;
+    mpu.TypeExtField     = MPU_TEX_LEVEL1;
+    mpu.AccessPermission = MPU_REGION_FULL_ACCESS;
+    mpu.DisableExec      = MPU_INSTRUCTION_ACCESS_ENABLE;
+    mpu.IsShareable      = MPU_ACCESS_NOT_SHAREABLE;
+    mpu.IsCacheable      = MPU_ACCESS_NOT_CACHEABLE;
+    mpu.IsBufferable     = MPU_ACCESS_NOT_BUFFERABLE;
+
+    HAL_MPU_ConfigRegion(&mpu);
+    HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
+}
+#endif
+
 void rhs_hal_cortex_init_early(void)
 {
 #ifdef STM32F765xx
+    rhs_hal_cortex_configure_mpu();
     SCB_EnableICache();
     SCB_EnableDCache();
 #endif
